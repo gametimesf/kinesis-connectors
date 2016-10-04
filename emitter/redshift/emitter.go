811b00e96f13b0d3b6accc30b9c8f32ce6d74a4d
Shoumitra Srivastava <shoumitra362@gmail.com>
2016-10-03 17:15:11 -0700

    Added functionality to use IAM roles (#3)
    * Added functionality to use IAM roles
    
    * Added s3 region to struct, renamed IAM to IAMRole

--- emitter/redshift/emitter.go
+++ emitter/redshift/emitter.go
@@ -16,11 +16,13 @@ import (
 type RedshiftEmitter struct {
 	AwsAccessKey       string
 	AwsSecretAccessKey string
+	IAMRole			   string
 	Delimiter          string
 	Format             string
 	Jsonpaths          string
 	S3Bucket           string
 	S3Prefix           string
+	S3Region		   string
 	TableName          string
 	Db                 *sql.DB
 }
@@ -29,7 +31,7 @@ type RedshiftEmitter struct {
 // then issues a copy command to Redshift data store.
 func (e RedshiftEmitter) Emit(s3Key string, b io.ReadSeeker) error {
 	// put contents to S3 Bucket
-	s3 := &Emitter{Bucket: e.S3Bucket}
+	s3 := &Emitter{Bucket: e.S3Bucket, Region: e.S3Region}
 	err := s3.Emit(s3Key, b)
 
 	if err != nil {
@@ -67,8 +69,13 @@ func (e RedshiftEmitter) copyStatement(s3Key string) string {
 	b := new(bytes.Buffer)
 	b.WriteString(fmt.Sprintf("COPY %v ", e.TableName))
 	b.WriteString(fmt.Sprintf("FROM 's3://%v/%v' ", e.S3Bucket, s3Key))
-	b.WriteString(fmt.Sprintf("CREDENTIALS 'aws_access_key_id=%v;", e.AwsAccessKey))
-	b.WriteString(fmt.Sprintf("aws_secret_access_key=%v' ", e.AwsSecretAccessKey))
+
+	if e.IAMRole != "" {
+		b.WriteString(fmt.Sprintf("CREDENTIALS '%v' ", e.IAM))
+	} else {
+		b.WriteString(fmt.Sprintf("CREDENTIALS 'aws_access_key_id=%v;", e.AwsAccessKey))
+		b.WriteString(fmt.Sprintf("aws_secret_access_key=%v' ", e.AwsSecretAccessKey))
+	}
 
 	switch e.Format {
 	case "json":
